5fe6f12790420887ec85077348b18703eb07faf1
--global <--global>
2025-11-24 16:25:40 +0530

    chore: create docker compose for production

--- order-execution-engine/src/helpers/logger.ts
+++ order-execution-engine/src/helpers/logger.ts
@@ -1,15 +1,16 @@
 import pino from 'pino';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport:
-    process.env.NODE_ENV === 'development'
-      ? undefined
-      : {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-          },
+  transport: !isProd
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
         },
+      }
+    : undefined, // <-- No pretty transport in production
 });
