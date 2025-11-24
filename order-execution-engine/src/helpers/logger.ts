efd14a2183ce6528b9471092a220fde4c25d7658
--global <--global>
2025-11-24 16:37:10 +0530

    bug: remove pino-pretty logging

--- order-execution-engine/src/helpers/logger.ts
+++ order-execution-engine/src/helpers/logger.ts
@@ -4,13 +4,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: !isProd
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'SYS:standard',
-        },
-      }
-    : undefined, // <-- No pretty transport in production
+  transport: undefined, // <-- No pretty transport in production
 });
